17c577bb387b9faf968a4a6be318786f4e511f5c
bruceryu77 <bruceryu77@gmail.com>
2026-06-26 17:43:06 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ultra-order-cache-v12';
+const CACHE_NAME = 'ultra-order-cache-v13';
 
 const FILES_TO_CACHE = [
   './',
@@ -37,3 +37,4 @@ self.addEventListener('fetch', event => {
     caches.match(event.request).then(response => response || fetch(event.request))
   );
 });
+
